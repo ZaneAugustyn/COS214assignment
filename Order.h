7927b129c9c5d636707ef05b8b983eb6c5b7c780
Johane-B <JohAnkia@2>
2023-11-03 00:18:11 +0200

    Added doxygen comments for Order class

--- Order.h
+++ Order.h
@@ -14,24 +14,63 @@ using namespace std;
 
 class Order : public OrderComponent {
 private:
-    Group* group_;
-    vector<OrderComponent*> components_;
-    int currentComponentIndex_;
+    Group* group_; /**< Pointer to the group associated with this order. */
+    vector<OrderComponent*> components_; /**< List of order components within this order. */
+    int currentComponentIndex_; /**< Index of the current order component when iterating. */
     friend class GroupIterator;
 public:
+    /**
+     * @brief Constructor for an Order.
+     * @param group The group to which this order belongs.
+     */
     Order(Group* group);
+
+    /**
+     * @brief Destructor for an Order.
+     */
     ~Order();
+
+    /**
+     * @brief Calculate and return the total price of the order.
+     * @return The total price of the order.
+     */
     double getPrice();
+
+    /**
+     * @brief Add an order component to this order.
+     * @param component The order component to add.
+     */
     void addComponent(OrderComponent* component);
+
+    /**
+     * @brief Remove an order component from this order.
+     * @param component The order component to remove.
+     */
     void removeComponent(OrderComponent* component);
+
+    /**
+     * @brief Get a vector of all order components within this order.
+     * @return A vector of order components.
+     */
     virtual vector<OrderComponent*> getItems();
+
+    /**
+     * @brief Create an iterator for traversing the order components.
+     * @return A GroupIterator for this order.
+     */
     GroupIterator* createIterator();
-    // Order* getNextComponent();
-    // bool isDone();
-    // Order* getCurrentComponent();
-    int getGroupNumber();
-    Group* getGroup();
     
+    /**
+     * @brief Get the group number associated with this order.
+     * @return The group number.
+     */
+    int getGroupNumber();
+
+    /**
+     * @brief Get the group associated with this order.
+     * @return A pointer to the associated group.
+     */
+    Group* getGroup();
 };
 
 #endif
