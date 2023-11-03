6e4a4a8fe62efc4897c9dca9c8db54d4d3176d65
Keanu Canto <133786470+Keanumrc@users.noreply.github.com>
2023-11-03 09:04:55 +0200

    Added doxygen comments to Bun.h

--- Bun.h
+++ Bun.h
@@ -3,11 +3,26 @@
 
 #include "OrderItem.h"
 
+/**
+ * @class Bun
+ * @brief This class inherits from OrderItem and represents a burger bun to be served in the restaurant
+*/
+
 class Bun : public OrderItem
 {
 private:
 public:
+
+    /**
+     * @brief default constructor - sets OrderItem's attributes to the values of a bun
+     * @return none
+    */
     Bun();
+
+    /**
+     * @brief destructor
+     * @return none
+    */
     ~Bun();
 };
 
