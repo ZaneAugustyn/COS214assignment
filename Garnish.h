2127d2a80b45b07f48a58e911159978c243b7adb
Keanu Canto <133786470+Keanumrc@users.noreply.github.com>
2023-11-03 09:43:03 +0200

    added doxygen comments to garnish hierarchy

--- Garnish.h
+++ Garnish.h
@@ -3,11 +3,28 @@
 
 #include "OrderItem.h"
 
+/**
+ * @class Garnish
+ * @brief This class inherits from OrderItem and represents garnish that a guest can have on their burger
+*/
+
 class Garnish : public OrderItem
 {
 private:
 public:
+
+    /**
+     * @brief constructor
+     * @param price takes in the price of the Side
+     * @param name takes in a name for the Side
+     * @return none
+    */
     Garnish(double price, string name);
+
+    /**
+     * @brief destructor
+     * @return none
+    */
     ~Garnish();
 };
 
--- Tomato.h
+++ Tomato.h
@@ -3,11 +3,26 @@
 
 #include "Garnish.h"
 
+/**
+ * @class Tomato
+ * @brief This class inherits from Garnish and represents tomato to garnish a burger
+*/
+
 class Tomato : public Garnish
 {
 private:
 public:
+
+    /**
+     * @brief default constructor - sets Garnish's attributes to the values of tomato
+     * @return none
+    */
     Tomato();
+
+    /**
+     * @brief destructor
+     * @return none
+    */
     ~Tomato();
 };
 
