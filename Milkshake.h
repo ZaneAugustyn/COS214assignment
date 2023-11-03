eeb057221a2ecb955f59d6aa12668531398f746e
Keanu Canto <133786470+Keanumrc@users.noreply.github.com>
2023-11-03 09:48:14 +0200

    added doxygen comments for the drinks hierarchy

--- Milkshake.h
+++ Milkshake.h
@@ -3,11 +3,26 @@
 
 #include "Drink.h"
 
+/**
+ * @class Milkshake
+ * @brief This class inherits from Drink and represents a milkshake
+*/
+
 class Milkshake : public Drink
 {
 private:
 public:
+
+    /**
+     * @brief default constructor - sets Drink's attributes to the values of a milkshake
+     * @return none
+    */
     Milkshake();
+
+    /**
+     * @brief destructor
+     * @return none
+    */
     ~Milkshake();
 };
 
