fc0fde8c57f9db87f7a7c2790891f0d9bf8e67d7
Johane-B <JohAnkia@2>
2023-11-04 18:23:25 +0200

    Added doxygen comments for MakeTab and Plate

--- MakeTab.h
+++ MakeTab.h
@@ -9,15 +9,42 @@ using namespace std;
 
 class Group;
 
-class MakeTab: public BillPayment
-{
-private:
-    
+/**
+ * @class MakeTab
+ * @brief A class for handling tab payments in a group.
+ */
+class MakeTab : public BillPayment {
 public:
+    /**
+     * @brief Constructor for creating a MakeTab instance.
+     * @param t The total amount for the tab.
+     */
     MakeTab(float t);
+
+    /**
+     * @brief Destructor for the MakeTab instance.
+     */
     ~MakeTab();
+
+    /**
+     * @brief Create a tab with the specified total amount.
+     * @param t The total amount for the tab.
+     * @return The created Tab instance.
+     */
     Tab* createTab(float t);
+
+    /**
+     * @brief Handle the tab payment for a group.
+     * @param group The group for which the tab is being created.
+     * @return void
+     */
     void Pay(Group* group);
+
+    /**
+     * @brief Set the tab amount to a specified Tab instance.
+     * @param tab The Tab instance to set the total amount for the tab.
+     * @return void
+     */
     void setTab(Tab* tab);
 };
 
--- Plate.h
+++ Plate.h
@@ -7,15 +7,37 @@ class OrderItem;
 
 using namespace std;
 
-class Plate
-{
-  private:
-    Order* order_;
-    vector<OrderItem*> burger_;
-  public:
+/**
+ * @class Plate
+ * @brief A class representing a plate containing a list of order items.
+ */
+class Plate {
+private:
+    Order* order_; 
+    vector<OrderItem*> burger_; 
+
+public:
+    /**
+     * @brief Constructor for creating a Plate instance.
+     */
     Plate();
+
+    /**
+     * @brief Print the contents of the plate to the console.
+     * @return void
+     */
     void printPlate();
-    void addToPlate(OrderItem*);
+
+    /**
+     * @brief Add an order item to the plate.
+     * @param item The order item to add to the plate.
+     * @return void
+     */
+    void addToPlate(OrderItem* item);
+
+    /**
+     * @brief Destructor for the Plate instance.
+     */
     virtual ~Plate();
 };
 
