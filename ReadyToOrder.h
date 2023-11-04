6175ababba78e0138a1ebcf59dda86e733083271
Zane Augustyn <86051512+ZaneAugustyn@users.noreply.github.com>
2023-11-04 07:38:27 +0200

    Update ReadyToOrder.h

--- ReadyToOrder.h
+++ ReadyToOrder.h
@@ -9,14 +9,62 @@ class ReadyToOrder : public GroupState
 private:
     string stateName_;
 public:
+    /**
+     * @brief constructor
+    */
     ReadyToOrder();
+
+    /**
+     * @brief destructor
+    */
     ~ReadyToOrder();
+
+    /**
+     * @brief the ConcreteClass method to handle the RequestToOrder request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void RequestToOrder(Group * group);
+
+    /**
+     * @brief the ConcreteClass method to handle the WaitInQueue request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void WaitInQueue(Group * group);
+
+    /**
+     * @brief the ConcreteClass method to handle the MakeUnhappy request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void MakeUnhappy(Group * group);
+
+    /**
+     * @brief the ConcreteClass method to handle the MakeHappy request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void MakeHappy(Group * group);
+
+    /**
+     * @brief the ConcreteClass method to handle the RequestTab request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void RequestTab(Group * group);
+
+    /**
+     * @brief the ConcreteClass method to handle the RequestBill request
+     * @param group the group to update the state of
+     * @return void  
+    */
     virtual void RequestBill(Group * group);
+
+    /**
+     * @brief method to return a string of the state name
+     * @return string
+    */
     virtual string ToString();
 };
 
