7f2ca29fb4849a62176271e4c19c366476a9124f
LaylaOlivier <u21433373@tuks.co.za>
2023-11-04 17:28:09 +0200

    Added the Neutral state to Group States

---
+++ Neutral.cpp
@@ -0,0 +1,55 @@
+#include "Neutral.h"
+#include "Happy.h"
+#include "ReadyToOrder.h"
+#include "Unhappy.h"
+#include "ReadyForBill.h"
+
+Neutral::Neutral()
+{
+    stateName_ = "Neutral";
+    cout << "State changed to " << stateName_ << endl;
+}
+
+Neutral::~Neutral()
+{
+}
+
+void Neutral::RequestToOrder(Group *group)
+{
+    group->SetState(new ReadyToOrder());
+}
+
+void Neutral::WaitInQueue(Group *group)
+{
+    cout << "Cannot move to WaitForTable state." << endl;
+}
+
+void Neutral::MakeUnhappy(Group *group)
+{
+    group->SetState(new Unhappy());
+}
+
+void Neutral::MakeHappy(Group * group) 
+{
+    group->SetState(new Happy());
+}
+
+void Neutral::MakeNeutral(Group *group)
+{
+    cout << "Already in Neutral state." << endl;
+}
+
+void Neutral::RequestTab(Group *group)
+{
+    cout << "Cannot request a tab when seated." << endl;
+}
+
+void Neutral::RequestBill(Group *group)
+{
+    group->SetState(new ReadyForBill());
+}
+
+string Neutral::ToString()
+{
+    return this->stateName_;
+}
