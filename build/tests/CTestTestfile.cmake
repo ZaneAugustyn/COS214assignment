804c4a7bb72dfb8916d7bdca761efadbbd998d6f
NotYourX <xadrianvheerden@gmail.com>
2023-11-07 13:56:27 +0200

    make

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xadrian/COS 214/GroupProject/COS214assignment/tests
-# Build directory: /home/xadrian/COS 214/GroupProject/COS214assignment/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/xadrian/COS 214/GroupProject/COS214assignment/build/tests/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/xadrian/COS 214/GroupProject/COS214assignment/tests/CMakeLists.txt;16;add_test;/home/xadrian/COS 214/GroupProject/COS214assignment/tests/CMakeLists.txt;0;")
