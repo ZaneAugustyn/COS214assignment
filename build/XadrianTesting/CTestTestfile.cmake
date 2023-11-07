40759831400162b149ebc008903f6d2bc2a49df4
NotYourX <xadrianvheerden@gmail.com>
2023-11-07 13:31:51 +0200

    make

--- build/XadrianTesting/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xadrian/COS 214/GroupProject/COS214assignment/XadrianTesting
-# Build directory: /home/xadrian/COS 214/GroupProject/COS214assignment/build/XadrianTesting
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(XadrianTesting "/home/xadrian/COS 214/GroupProject/COS214assignment/build/XadrianTesting/XadrianTesting")
-set_tests_properties(XadrianTesting PROPERTIES  _BACKTRACE_TRIPLES "/home/xadrian/COS 214/GroupProject/COS214assignment/XadrianTesting/CMakeLists.txt;16;add_test;/home/xadrian/COS 214/GroupProject/COS214assignment/XadrianTesting/CMakeLists.txt;0;")
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xadrian/COS 214/GroupProject/COS214assignment/googletest/googlemock
-# Build directory: /home/xadrian/COS 214/GroupProject/COS214assignment/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
