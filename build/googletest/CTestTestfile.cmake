e20ae188b734705baafce029431f3fac3d6fad7f
NotYourX <xadrianvheerden@gmail.com>
2023-11-07 13:50:45 +0200

    make

---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/xadrian/COS 214/GroupProject/COS214assignment/googletest
+# Build directory: /home/xadrian/COS 214/GroupProject/COS214assignment/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
